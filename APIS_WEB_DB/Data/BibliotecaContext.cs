9c1df59bca648116b55c59c9127ef63e745231f5
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-24 02:44:47 +0200

    Appsetting.json
    Luego de instalar mis nugets necesarias cree mi conexión a la base de datos

--- APIS_WEB_DB/Data/BibliotecaContext.cs
+++ APIS_WEB_DB/Data/BibliotecaContext.cs
@@ -1,6 +1,15 @@
-﻿namespace APIS_WEB_DB.Data
+﻿using APIS_WEB_DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIS_WEB_DB.Data
 {
-    public class BibliotecaContext
+    public class BibliotecaContext : DbContext
     {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Libro> Libros { get; set; }
     }
 }
--- APIS_WEB_DB/Models/Libro.cs
+++ APIS_WEB_DB/Models/Libro.cs
@@ -2,5 +2,13 @@
 {
     public class Libro
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnioPublicacion { get; set; }
+        public string Genero { get; set; }
+        public int NumeroPaginas { get; set; }
+        public decimal Precio { get; set; }
+        public bool Disponible { get; set; }
     }
 }
