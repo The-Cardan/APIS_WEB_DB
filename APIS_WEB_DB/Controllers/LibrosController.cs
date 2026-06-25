bd11c619f81be0047cf2938875a77668d7506a30
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-25 02:14:41 +0200

    LibrosController.cs/Get
    Este primer endpoint me devuelve todos los libros existentes en mi DB.

---
+++ APIS_WEB_DB/Controllers/LibrosController.cs
@@ -0,0 +1,30 @@
+﻿using APIS_WEB_DB.Data;
+using APIS_WEB_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIS_WEB_DB.Controllers
+{
+    [Route("api/libros")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly BibliotecaContext _context;
+
+        public LibrosController(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+
+        //Optener todos los libros
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libro>>> GetLibros()
+        {
+            return await _context.Libros.ToListAsync();
+        }
+
+       
+
+    }
+}
