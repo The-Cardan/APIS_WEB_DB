24c006ff28c201fe8f41343c939a239205015364
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-07-16 01:43:29 +0200

    Update lLibro.cs
    Esto para que ahora en vez de la clase guardar los libros en nombre del autor, sea en el id del autor

--- APIS_WEB_DB/Models/Libro.cs
+++ APIS_WEB_DB/Models/Libro.cs
@@ -1,14 +1,31 @@
-﻿namespace APIS_WEB_DB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIS_WEB_DB.Models
 {
     public class Libro
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
         public string Titulo { get; set; }
-        public string Autor { get; set; }
+
+        [Range(1450, 2100)]
         public int AnioPublicacion { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Genero { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int NumeroPaginas { get; set; }
+
+        [Range(0, double.MaxValue)]
         public decimal Precio { get; set; }
+
         public bool Disponible { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int AutorId { get; set; }
     }
 }
