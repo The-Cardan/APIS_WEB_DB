68ef8499252a37f46c2c28ad8c627f5c88cb2dcc
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-07-16 01:38:39 +0200

    Create Autor.cs
    Esta clase es para que ahora los libros se guarden en el id del autor

---
+++ APIS_WEB_DB/Models/Autor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIS_WEB_DB.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nacionalidad { get; set; }
+
+        [Range(1500, 2100)]
+        public int AnioNacimiento { get; set; }
+    }
+}
