1896dcb3b077ac06623a91ab89e7e521e5d48644
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-07-16 02:40:05 +0200

    Create AutorController.cs
    Este controller es el que va a manejar las nuevas peticiones que voy a hacer respecto a los autores

---
+++ APIS_WEB_DB/Controllers/AutoresController.cs
@@ -0,0 +1,19 @@
+﻿using APIS_WEB_DB.Data;
+using APIS_WEB_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIS_WEB_DB.Controllers
+{
+    [Route("api/autores")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly BibliotecaContext _context;
+
+        public AutoresController(BibliotecaContext context)
+        {
+            _context = context;
+        }
+    }
+}
